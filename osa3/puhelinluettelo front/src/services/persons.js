beb18c5b350ecb3deb1a809929eb780060ea98a4
joelkiuru <joelkiuru@gmail.com>
2023-09-12 14:51:30 +0300

    production build

--- osa3/puhelinluettelo front/src/services/persons.js
+++ osa3/puhelinluettelo front/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://render-testing-629h.onrender.com/api/persons'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request =  axios.get(baseUrl)
