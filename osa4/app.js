0e7d0f9bd84c88cdc2c36f85140eb4bc17dea4ee
joelkiuru <joelkiuru@gmail.com>
2023-11-06 22:03:18 +0200

    finished controller, config, model

---
+++ osa4/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+// const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const Blog = require('./models/blog')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
